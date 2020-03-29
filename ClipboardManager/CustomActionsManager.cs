426d60cb5e62def45a76d720546429a682721abb
ogendogen <kosmos500@interia.pl>
2020-03-29 21:39:43 +0200

    Added CustomActionsManager with OpenURL method

---
+++ ClipboardManager/CustomActionsManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Core
+{
+    public class CustomActionsManager
+    {
+        public void OpenURL(string url)
+        {
+            Process.Start(url);
+        }
+    }
+}
