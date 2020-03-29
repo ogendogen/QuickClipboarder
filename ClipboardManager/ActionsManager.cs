1af9a78ff91e7d783f074b6ccd1d9282a667275a
ogendogen <kosmos500@interia.pl>
2020-03-29 21:47:25 +0200

    Implemented ActionsManager as a public API

--- ClipboardManager/ActionsManager.cs
+++ ClipboardManager/ActionsManager.cs
@@ -6,6 +6,37 @@ namespace Core
 {
     public class ActionsManager
     {
+        internal ClipboardManager ClipboardManager { get; set; }
+        internal CustomActionsManager CustomActionsManager { get; set; }
 
+        public ActionsManager(DataManager.DataManager dataManager)
+        {
+            ClipboardManager = new ClipboardManager(dataManager);
+            CustomActionsManager = new CustomActionsManager();
+        }
+        public void CopyTextToClipboard(string text)
+        {
+            ClipboardManager.CopyTextToClipboard(text);
+        }
+
+        public void CopyImageToClipboard(string path)
+        {
+            ClipboardManager.CopyImageToClipboard(path);
+        }
+
+        public void CopyFileToClipboard(string path)
+        {
+            ClipboardManager.CopyFileToClipboard(path);
+        }
+
+        public void CopyURLToClipboard(string url)
+        {
+            ClipboardManager.CopyURLToClipboard(url);
+        }
+
+        public void OpenURL(string url)
+        {
+            CustomActionsManager.OpenURL(url);
+        }
     }
 }
