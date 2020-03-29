3d5e320d452a97e102f5cd1921c2f48769ff1897
ogendogen <kosmos500@interia.pl>
2020-03-29 22:52:12 +0200

    Implemented MenuBuilder

--- QuickClipboarder/MenuBuilder.cs
+++ QuickClipboarder/MenuBuilder.cs
@@ -1,11 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Forms;
+using Models;
 
 namespace QuickClipboarder
 {
     public class MenuBuilder
     {
+        public List<Event> Events { get; set; }
+        public EventHandler ItemClickedEvent { get; set; }
+        public MenuBuilder(EventHandler eventHandler)
+        {
+            Events = new List<Event>();
+            ItemClickedEvent = eventHandler;
+        }
 
+        public void AddEvents(List<Event> events)
+        {
+            Events.AddRange(events);
+        }
+
+        public void Clear()
+        {
+            Events.Clear();
+        }
+
+        public ToolStripMenuItem Build()
+        {
+            ToolStripMenuItem item = new ToolStripMenuItem("Menu");
+
+            foreach (var eventItem in Events)
+            {
+                item.DropDownItems.Add(eventItem.Name, null, ItemClickedEvent);
+            }
+
+            return item;
+        }
     }
 }
